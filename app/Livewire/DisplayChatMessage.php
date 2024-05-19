b4110d9d88c28c0f162aeb07a3901c25aed040d8
raccoon254 <tomsteve187@gmail.com>
2024-05-19 15:08:18 +0300

    Receive messages from the current message component

--- app/Livewire/DisplayChatMessage.php
+++ app/Livewire/DisplayChatMessage.php
@@ -2,11 +2,20 @@
 
 namespace App\Livewire;
 
+use App\Models\Message;
+use Illuminate\View\View;
 use Livewire\Component;
 
 class DisplayChatMessage extends Component
 {
-    public function render()
+    public Message $message;
+
+    public function mount(Message $message): void
+    {
+        $this->message = $message;
+    }
+
+    public function render(): View
     {
         return view('livewire.display-chat-message');
     }
