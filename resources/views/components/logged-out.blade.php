bf591d5a779485845975677b653e6cf66b439d26
raccoon254 <tomsteve187@gmail.com>
2024-05-27 10:34:15 +0300

    Created logged layout and about page

---
+++ resources/views/components/logged-out.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>
+        {{ $title ?? 'Scholarspace' }}
+    </title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet"/>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+</head>
+<body class="font-sans min-h-screen bg-gray-50 antialiased">
+@include('layouts.navigation.logged-out')
+{{ $slot }}
+</body>
+</html>
--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -11,13 +11,6 @@
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet"/>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-
-    <style>
-        .custom-btn {
-            @apply bg-blue-500 text-white hover:bg-blue-600 font-semibold py-2 px-4 rounded-md;
-        }
-    </style>
-
 </head>
 <body class="font-sans min-h-screen bg-gray-50 antialiased">
 @include('layouts.navigation.logged-out')
