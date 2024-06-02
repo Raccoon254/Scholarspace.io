3db88c014f73f8553a43d3be8daff27382ea4ead
raccoon254 <tomsteve187@gmail.com>
2024-06-02 15:14:30 +0300

    Fixed order show component

--- resources/views/orders/show.blade.php
+++ resources/views/orders/show.blade.php
@@ -8,7 +8,7 @@
 
     <div class="max-w-7xl relative h-full mx-auto">
         <div class="flex sm:mx-3 lg:mx-4 flex-col h-full md:flex-row">
-            <div class="flex flex-col h-[85vh] text-black/70  gap-4">
+            <div class="flex flex-col h-[85vh] text-black/70 gap-4">
                 @if(!$orders->isEmpty())
                     <div class="flex bg-white p-4 rounded-lg justify-between items-center gap-4">
                         <div class="flex gap-4">
@@ -61,48 +61,49 @@
                                 </span>
                         </div>
                     @else
-                            <table class="table my-4 ring-1 ring-gray-50 rounded-md overflow-clip">
-                                <thead>
-                                <tr class="border-gray-100">
-                                    @foreach(['title' => 'Orders', 'description' => 'Description', 'total_price' => 'Total Price', 'status' => 'Status'] as $field => $label)
-                                        <th onselectstart="return false"
-                                            class="text-black/80 text-[14px] cursor-pointer"
-                                            wire:click="sortBy('{{ $field }}')">
-                                            {{ $label }}
-                                            <i class="fas {{ $this->getSortIcon($field) }}"></i>
-                                        </th>
-                                    @endforeach
-                                    <th class="text-black/80 text-[14px]">Payment</th>
-                                </tr>
-                                </thead>
-                                <tbody>
-                                @foreach($orders as $order)
-                                    <tr class="border-b border-gray-100">
-                                        <td class="">{{ $order->title }}</td>
-                                        <td class="">{{ $order->description }}</td>
-                                        <td class="">$ {{ $order->total_price }}</td>
-                                        <td class="">
-                                            <span class="rounded-md grid place-items-center px-2 py-1 {{ $order->getStatusClass() }} text-white">
+                        <table class="table my-4 ring-1 ring-gray-50 rounded-md overflow-clip">
+                            <thead>
+                            <tr class="border-gray-100">
+                                @foreach(['title' => 'Orders', 'description' => 'Description', 'total_price' => 'Total Price', 'status' => 'Status'] as $field => $label)
+                                    <th onselectstart="return false"
+                                        class="text-black/80 text-[14px] cursor-pointer"
+                                        wire:click="sortBy('{{ $field }}')">
+                                        {{ $label }}
+                                        <i class="fas {{ $this->getSortIcon($field) }}"></i>
+                                    </th>
+                                @endforeach
+                                <th class="text-black/80 text-[14px]">Payment</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            @foreach($orders as $order)
+                                <tr class="border-b border-gray-100">
+                                    <td class="">{{ $order->title }}</td>
+                                    <td class="">{{ $order->description }}</td>
+                                    <td class="">$ {{ $order->total_price }}</td>
+                                    <td class="">
+                                            <span
+                                                class="rounded-md grid place-items-center px-2 py-1 {{ $order->getStatusClass() }} text-white">
                                                 {{ $order->status }}
                                             </span>
-                                        </td>
-                                        <td class="">
+                                    </td>
+                                    <td class="flex">
                                             <span
                                                 class="rounded-md place-items-center px-2 py-1 {{ $order->isPaid() ? $order->payment->getStatusClass() : 'bg-red-500' }} text-white">
                                                 {{ $order->isPaid() ? $order->payment->status : 'Not Paid' }}
                                             </span>
-                                            <!-- Show payment button if order is not paid -->
-                                            @if(!$order->isPaid())
-                                                <button wire:click="payOrder({{ $order->id }})"
-                                                        class="p-2 mx-4 px-4 bg-blue-500 text-white font-semibold rounded-md">
-                                                    Pay
-                                                </button>
-                                            @endif
-                                        </td>
-                                    </tr>
-                                @endforeach
-                                </tbody>
-                            </table>
+                                        <!-- Show payment button if order is not paid -->
+                                        @if(!$order->isPaid())
+                                            <button wire:click="payOrder({{ $order->id }})"
+                                                    class="p-2 mx-4 px-4 bg-blue-500 text-white font-semibold rounded-md">
+                                                Pay
+                                            </button>
+                                        @endif
+                                    </td>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
 
                         <!-- Pagination -->
                         <div class="mt-4">
