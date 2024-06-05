e733574e21da673ca7fd470a4ec5048470617678
raccoon254 <tomsteve187@gmail.com>
2024-06-05 18:30:13 +0300

    Added open and close bars to the navigation bar on small screens

--- resources/views/layouts/navigation/logged-out.blade.php
+++ resources/views/layouts/navigation/logged-out.blade.php
@@ -16,10 +16,30 @@
                    class="bg-green-500 text-black/90 hover:bg-green-600 font-semibold py-2 px-4 rounded-md">Register</a>
             </div>
             <div class="md:hidden">
-                <button>
-                    <i class="fas fa-bars"></i>
+                <button @click="open = !open">
+                    <i x-show="!open" class="fas fa-bars"></i>
+                    <i x-show="open" class="fas fa-times"></i>
                 </button>
             </div>
         </div>
+        <div class="md:hidden flex flex-col gap-1 w-full p-1" x-show="open" @click.away="open = false">
+            <a href="{{ route('home') }}" class="block py-1 text-black/50 hover:text-black">Home</a>
+            <a href="{{ route('how-it-works') }}" class="block py-1 text-black/50 hover:text-black">How It Works</a>
+            <a href="{{ route('services') }}" class="block py-1 text-black/50 hover:text-black">Services</a>
+            <a href="{{ route('about') }}" class="block py-1 text-black/50 hover:text-black">About Us</a>
+            <a href="{{ route('contact') }}" class="block py-1 text-black/50 hover:text-black">Contact</a>
+            <a href="{{ route('login') }}"
+               class="block py-1 px-2 bg-blue-500 text-white/90 hover:bg-blue-600 font-semibold rounded-md">Login</a>
+            <a href="{{ route('register') }}"
+               class="block py-1 px-2 bg-green-500 text-black/90 hover:bg-green-600 font-semibold rounded-md">Register</a>
+        </div>
     </div>
 </nav>
+
+<script>
+    function dropdown() {
+        return {
+            open: false,
+        }
+    }
+</script>
