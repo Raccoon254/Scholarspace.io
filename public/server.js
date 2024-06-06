b47f920a095bb6308d7498ba9f8dbfc400748c24
raccoon254 <tomsteve187@gmail.com>
2024-06-06 17:06:10 +0300

    Added hello world to test on the server js file

--- public/server.js
+++ public/server.js
@@ -83,6 +83,10 @@ app.get('/online-users', (req, res) => {
     res.json(Array.from(connectedUsers.values()));
 });
 
+app.get('test',(req,res)=> {
+    res.send('Hello World')
+});
+
 server.listen(3000, () => {
     console.log('Server running on port 3000');
 });
