6a1fe2b52caa981d77345c431797db018cdd037e
raccoon254 <tomsteve187@gmail.com>
2024-06-13 18:29:05 +0300

    Added phone and location to the user model as fillables

--- app/Models/User.php
+++ app/Models/User.php
@@ -22,6 +22,8 @@ class User extends Authenticatable implements MustVerifyEmail
         'name',
         'email',
         'role',
+        'phone',
+        'location',
         'profile_photo',
         'password',
     ];
