fcc37e568a31ca768276d4efc93dc910039973e4
raccoon254 <tomsteve187@gmail.com>
2024-06-13 18:34:23 +0300

    Added phone and location fields to the migration

--- database/migrations/2024_06_13_153122_add_phone_and_location_to_users_table.php
+++ database/migrations/2024_06_13_153122_add_phone_and_location_to_users_table.php
@@ -12,7 +12,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->string('phone')->nullable()->after('role');
+            $table->string('location')->nullable()->after('phone');
         });
     }
 
@@ -22,7 +23,8 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->dropColumn('phone');
+            $table->dropColumn('location');
         });
     }
 };
