541f29de8ed6894451ea0eb85ebf7c5b0ee1e2aa
raccoon254 <tomsteve187@gmail.com>
2024-06-13 19:03:56 +0300

    Fixed welcome notification to the user who joins

--- app/Notifications/UserWelcome.php
+++ app/Notifications/UserWelcome.php
@@ -2,6 +2,7 @@
 
 namespace App\Notifications;
 
+use App\Models\User;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
@@ -10,15 +11,15 @@ use Illuminate\Notifications\Notification;
 class UserWelcome extends Notification
 {
     use Queueable;
+    protected User $user;
 
     /**
      * Create a new notification instance.
      */
-    public function __construct()
+    public function __construct(User $user)
     {
-        //
+        $this->user = $user;
     }
-
     /**
      * Get the notification's delivery channels.
      *
@@ -34,10 +35,14 @@ class UserWelcome extends Notification
      */
     public function toMail(object $notifiable): MailMessage
     {
+        // Welcome message for the user
         return (new MailMessage)
-                    ->line('The introduction to the notification.')
-                    ->action('Notification Action', url('/'))
-                    ->line('Thank you for using our application!');
+            ->subject('Welcome to Scholarspace')
+            ->greeting('Hello ' . $this->user->name . '!')
+            ->line('Welcome to Scholarspace, the best platform for academic writing services.')
+            ->line('We are excited to have you on board.')
+            ->action('View Profile', url(route('users.show', $this->user)))
+            ->line('Thank you for using our application!');
     }
 
     /**
@@ -48,7 +53,10 @@ class UserWelcome extends Notification
     public function toArray(object $notifiable): array
     {
         return [
-            //
+            'name' => $this->user->name,
+            'email' => $this->user->email,
+            'phone' => $this->user->phone ?? 'Not provided',
+            'location' => $this->user->location ?? 'Not provided',
         ];
     }
 }
