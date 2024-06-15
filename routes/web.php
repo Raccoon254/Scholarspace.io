caf51aaded231685ea4cf6c151f778a3a66730a0
raccoon254 <tomsteve187@gmail.com>
2024-06-15 12:19:59 +0300

    Added more pages to the sitemap xml file

--- routes/web.php
+++ routes/web.php
@@ -50,6 +50,7 @@ Route::get('/about', [PagesController::class, 'about'])->name('about');
 Route::get('/faq', [PagesController::class, 'faq'])->name('faq');
 Route::view('/coming-soon', 'static.coming-soon')->name('coming-soon');
 Route::fallback([PagesController::class, 'notfound'])->where('catchall', '.*');
+Route::get('/not-found', [PagesController::class, 'notfound'])->name('not-found');
 Route::get('/info/price-calculator', [PagesController::class, 'info_price_calculator'])->name('info.price-calculator');
 Route::view('/info/order/create', 'info.order.create')->name('info.order.create');
 //info.order.payment
