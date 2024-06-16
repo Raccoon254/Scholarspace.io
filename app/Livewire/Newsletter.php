bc0e615cb2657ed61c6ca88efbb03f37d161acc2
raccoon254 <tomsteve187@gmail.com>
2024-06-16 08:34:55 +0300

    Complete add user email to newsletter

--- app/Livewire/Newsletter.php
+++ app/Livewire/Newsletter.php
@@ -2,11 +2,29 @@
 
 namespace App\Livewire;
 
+use App\Models\Subscriber;
 use Illuminate\View\View;
 use Livewire\Component;
 
 class Newsletter extends Component
 {
+    public $email;
+
+    protected $rules = [
+        'email' => 'required|email|unique:subscribers,email',
+    ];
+
+    public function subscribe(): void
+    {
+        $this->validate();
+
+        Subscriber::create(['email' => $this->email]);
+
+        session()->flash('message', 'You have successfully subscribed to the newsletter!');
+
+        $this->reset('email');
+    }
+
     public function render(): View
     {
         return view('livewire.newsletter');
