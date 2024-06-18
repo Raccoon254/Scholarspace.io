a0c62c29636e281c332bc6cf8c10cf7a4514a905
raccoon254 <tomsteve187@gmail.com>
2024-06-18 15:30:07 +0300

    Add failed enum to payments table

--- database/migrations/2024_06_18_122735_addfailedtopayments.php
+++ database/migrations/2024_06_18_122735_addfailedtopayments.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -11,7 +12,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        //
+        Schema::table('payments', function (Blueprint $table) {
+            DB::statement("ALTER TABLE payments MODIFY COLUMN status ENUM('pending', 'completed', 'failed') DEFAULT 'pending'");
+        });
     }
 
     /**
@@ -19,6 +22,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        //
+        Schema::table('payments', function (Blueprint $table) {
+            DB::statement("ALTER TABLE payments MODIFY COLUMN status ENUM('pending', 'completed') DEFAULT 'pending'");
+        });
     }
 };
