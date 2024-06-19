92ce6a88f67bc0fe45683740e818a624bf137664
raccoon254 <tomsteve187@gmail.com>
2024-06-19 08:38:02 +0300

    Add column description to the order deliveries table

--- database/migrations/2024_06_19_053618_add_description_to_deliveries_table.php
+++ database/migrations/2024_06_19_053618_add_description_to_deliveries_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('deliveries', function (Blueprint $table) {
-            //
+            $table->text('description')->nullable()->after('status');
         });
     }
 
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('deliveries', function (Blueprint $table) {
-            //
+            $table->dropColumn('description');
         });
     }
 };
