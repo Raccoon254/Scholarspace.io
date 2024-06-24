7fec85cf0d80baa103f15b1681951f3e007fda01
raccoon254 <tomsteve187@gmail.com>
2024-06-24 11:51:07 +0300

    Created user activity model to track user activities across the website

---
+++ app/Models/UserActivity.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class UserActivity extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'activity',
+        'description',
+        'device',
+        'browser',
+        'ip',
+        'user_agent',
+    ];
+
+    /**
+     * Get the user that owns the activity.
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
---
+++ database/factories/UserActivityFactory.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\UserActivity;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class UserActivityFactory extends Factory
+{
+    protected $model = UserActivity::class;
+
+    public function definition(): array
+    {
+        return [
+
+        ];
+    }
+}
