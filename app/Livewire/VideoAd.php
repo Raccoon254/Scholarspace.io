7ced0ef058e4c1a07c1949003c32649e70118d14
raccoon254 <tomsteve187@gmail.com>
2024-07-06 08:40:47 +0300

    Livewire component for the ad video

---
+++ app/Livewire/VideoAd.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Livewire;
+
+use Illuminate\View\View;
+use Livewire\Component;
+
+class VideoAd extends Component
+{
+    public function render(): View
+    {
+        return view('livewire.video-ad');
+    }
+}
