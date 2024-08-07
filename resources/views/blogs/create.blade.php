030ec6958a8c771a4860571140d36b48c7210ead
raccoon254 <tomsteve187@gmail.com>
2024-08-07 18:16:12 +0300

    Return new line for every action

--- resources/views/blogs/create.blade.php
+++ resources/views/blogs/create.blade.php
@@ -1,34 +1,34 @@
 <div class="p-4 flex gap-4">
     <div class="w-1/2 flex flex-col">
         <div class="flex gap-[1px] mb-2">
-            <button id="addHeading1" class="btn rounded-none m-0 btn-sm bg-blue-200 border-none btn-ghost text-black">
+            <button id="addHeading1" class="control-btn">
                 H1
             </button>
-            <button id="addHeading2" class="btn rounded-none m-0 btn-sm bg-blue-200 border-none btn-ghost text-black">
+            <button id="addHeading2" class="control-btn">
                 H2
             </button>
-            <button id="addHeading3" class="btn rounded-none m-0 btn-sm bg-blue-200 border-none btn-ghost text-black">
+            <button id="addHeading3" class="control-btn">
                 H3
             </button>
-            <button id="addParagraph" class="btn rounded-none m-0 btn-sm bg-blue-200 border-none btn-ghost text-black">
+            <button id="addParagraph" class="control-btn">
                 <i class="fas fa-paragraph"></i> P
             </button>
-            <button id="addBold" class="btn rounded-none m-0 btn-sm bg-blue-200 border-none btn-ghost text-black">
+            <button id="addBold" class="control-btn">
                 <i class="fas fa-bold"></i>
             </button>
-            <button id="addList" class="btn rounded-none m-0 btn-sm bg-blue-200 border-none btn-ghost text-black">
+            <button id="addList" class="control-btn">
                 <i class="fas fa-list"></i>
             </button>
-            <button id="addLink" class="btn rounded-none m-0 btn-sm bg-blue-200 border-none btn-ghost text-black">
+            <button id="addLink" class="control-btn">
                 <i class="fas fa-link"></i>
             </button>
-            <button id="addImage" class="btn rounded-none m-0 btn-sm bg-blue-200 border-none btn-ghost text-black">
+            <button id="addImage" class="control-btn">
                 <i class="fas fa-image"></i>
             </button>
         </div>
         <textarea
             id="content"
-            wire:model="content"
+            wire:model.live="content"
             type="text" class="border w-full border-gray-300 p-2"
             rows="10"></textarea>
     </div>
@@ -44,25 +44,25 @@
 
 <script>
     document.getElementById('addHeading1').addEventListener('click', function() {
-        addToContent("# H1 ");
+        addToContent("\n# H1 ");
     });
     document.getElementById('addHeading2').addEventListener('click', function() {
-        addToContent("## H2 ");
+        addToContent("\n## H2 ");
     });
     document.getElementById('addHeading3').addEventListener('click', function() {
-        addToContent("### H3 ");
+        addToContent("\n### H3 ");
     });
     document.getElementById('addParagraph').addEventListener('click', function() {
         addToContent("\n\n");
     });
     document.getElementById('addBold').addEventListener('click', function() {
-        addToContent(" **bold text** ");
+        addToContent("\n **bold text** ");
     });
     document.getElementById('addList').addEventListener('click', function() {
-        addToContent("\n- list item");
+        addToContent("\n\n- list item");
     });
     document.getElementById('addLink').addEventListener('click', function() {
-        addToContent(" [link text](url) ");
+        addToContent("\n [link text](url) ");
     });
     document.getElementById('addImage').addEventListener('click', function() {
         addToContent(" ![alt text](image_url) ");
