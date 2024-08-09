061fcb1a21439a2281ef739e0e0930e4c745755a
raccoon254 <tomsteve187@gmail.com>
2024-08-09 08:20:31 +0300

    Fetch all the blogs and their images

--- app/Livewire/BlogRenderer.php
+++ app/Livewire/BlogRenderer.php
@@ -2,11 +2,21 @@
 
 namespace App\Livewire;
 
+use Illuminate\View\View;
 use Livewire\Component;
+use App\Models\Blog;
 
 class BlogRenderer extends Component
 {
-    public function render()
+    public $blogs;
+
+    public function mount(): void
+    {
+        // Fetch all blogs with their related users and images
+        $this->blogs = Blog::with(['user', 'images'])->orderBy('created_at', 'desc')->get();
+    }
+
+    public function render(): View
     {
         return view('livewire.blog-renderer');
     }
